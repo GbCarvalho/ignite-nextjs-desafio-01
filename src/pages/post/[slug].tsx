9ab2c4aa38e9551e5714a741fada71b7273d641a
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-04-05 16:14:21 -0300

    Did the post page and synced with the prismic API
    TO DO: Calculate the minutes to read the post and display it in the post header

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,10 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+
+import { FiClock, FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +32,68 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <img src={post.data.banner.url} alt="background" />
+        <div className={styles.post}>
+          <header>
+            <h1>{post.data.title}</h1>
+            <span>
+              <time>
+                <FiCalendar size={20} />
+                {format(new Date(post.first_publication_date), 'dd LLL yyyy', {
+                  locale: ptBR,
+                })}
+              </time>
+              <p>
+                <FiUser size={20} />
+                {post.data.author}
+              </p>
+              <time>
+                <FiClock size={20} />4 min
+              </time>
+            </span>
+          </header>
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+          <body>
+            {post.data.content.map(part => (
+              <>
+                <h1>{part.heading}</h1>
 
-//   // TODO
-// };
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(part.body),
+                  }}
+                />
+              </>
+            ))}
+          </body>
+        </div>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
+
+  //   // TODO
+
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
 
 //   // TODO
 // };
